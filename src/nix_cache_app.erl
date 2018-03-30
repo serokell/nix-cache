12aaf5e372e0e60698a41881cf559c16eebfd54c
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-03-30 13:24:37 +0000

    nix_cache_app: add NIX_CACHE_PORT for port configuration

--- src/nix_cache_app.erl
+++ src/nix_cache_app.erl
@@ -3,10 +3,13 @@
 
 -export([start/2, stop/1]).
 
+port() ->
+    list_to_integer(os:getenv(<<"NIX_CACHE_PORT">>, "17672")).
+
 start(_Type, _Args) ->
     {ok, DB} = nix_cache_hash:to_path_db(),
     Dispatch = cowboy_router:compile([{'_', [{'_', nix_cache_handler, DB}]}]),
-    {ok, _} = cowboy:start_clear(http, [{port, 17672}], #{env => #{dispatch => Dispatch}}),
+    {ok, _} = cowboy:start_clear(http, [{port, port()}], #{env => #{dispatch => Dispatch}}),
     nix_cache_sup:start_link().
 
 stop(_State) ->
